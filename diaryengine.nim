1dc049e45de81f51dcdbdc77bbe81b0daaf9b718
Sven Schober <sschober@sssm.de>
2017-08-06 15:41:32 +0200

    add type DiaryEntry, read the first few lines and output summary
    a few rough edges for me here: I don't quiet understand everything
    I've done here. but all in all I've refactored the code a bit: now,
    it's a two pass approach, first information is gathered in a sequence
    of DiaryEntry tuples and then the gathered information is output.
    
    what is missing is some kind of templating mechanism, to factor out
    the concern of presenting the information. for that, I need to switch
    to nimble, so that I can declare dependencies.

--- diaryengine.nim
+++ diaryengine.nim
@@ -14,15 +14,33 @@ import os
 import strutils
 import streams
 
+type 
+  DiaryEntry = tuple[path: string, title: string, abstract: string]
+
+var diaryEntries: seq[DiaryEntry] = @[]
+
 if paramCount()  > 0:
   for kind, path in walkdir(paramStr(1)):
-    if endsWith(toLowerAscii(path), ".mkd"):
-      echo(path)
+    if kind == pcFile and endsWith(toLowerAscii(path), ".mkd"):
+      var diaryEntry: DiaryEntry
+      diaryEntry.path = path
       let fs = newFileStream(path, fmRead)
       if not isNil(fs):
         var line = ""
         if fs.readLine(line):
-          echo line
+          diaryEntry.title = line
+        var i = 3
+        if nil == diaryEntry.abstract:
+          diaryEntry.abstract = ""
+        while i > 0 and len(diaryEntry.abstract) < 100 and fs.readLine(line):
+          diaryEntry.abstract.add(line)
+          inc(i)
         fs.close()
+        diaryEntries.add(diaryEntry)
 else:
   echo "usage: " & paramStr(0) & " <directory>"
+
+for i, diaryEntry in diaryEntries:
+  # TODO: why do I have to use a $ to access i here? is it the toString operator? 
+  # TODO: and why are commas neccessary?
+  echo "[", $i, "]: title: ", diaryEntry.title, "\n", diaryEntry.abstract
